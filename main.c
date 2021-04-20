7ec274ca2b6b4fdb91493cdf11f3a67319b69bfc
necromancerthedark <kg22584@gmail.com>
2021-04-20 22:17:35 +0530

    added destroy and rise window

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
+#include <X11/X.h>
 #include<X11/Xlib.h>
 #include <stdio.h>
 #include <X11/Xutil.h>
 
 int main(){
 		Display *dpy;
+		char *list;
 		Window root;
 		XWindowAttributes attr;
 		XButtonEvent bevent;
 		XEvent event; 
+		XKeyEvent kevent;
+		KeySym ks;
 
 
 		/*
@@ -23,8 +27,9 @@ int main(){
 		 * Grabs keyboard input to here we use Mod4Mask ,i.e., super key
 		 * as mod key and "a" as combination
 		 */
-		XGrabKey(dpy,XKeysymToKeycode(dpy,XStringToKeysym("a")),Mod4Mask,root,True,GrabModeAsync,GrabModeAsync);
+		XGrabKey(dpy,XKeysymToKeycode(dpy,XStringToKeysym("r")),Mod4Mask,root,True,GrabModeAsync,GrabModeAsync);
 
+		XGrabKey(dpy,XKeysymToKeycode(dpy,XStringToKeysym("c")),Mod4Mask,root,True,GrabModeAsync,GrabModeAsync);
 		/*
 		 * main event loop
 		 */
@@ -32,6 +37,18 @@ int main(){
 		while (True){
 				XNextEvent(dpy,&event);
 				switch(event.type){
+						/*If super+a is pressed we can raise a given window*/
+						case KeyPress:
+								kevent = event.xkey;
+								ks = XKeycodeToKeysym(dpy,kevent.keycode, 0);
+										if (event.xkey.subwindow){
+												if(ks==114)
+													XRaiseWindow(dpy,event.xkey.subwindow);
+												if(ks==99)
+													XDestroyWindow(dpy, event.xkey.subwindow);
+										}
+								
+								break;
 						case ButtonPress:
 										break;
 						case MotionNotify: break;
